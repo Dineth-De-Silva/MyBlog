2bb7f26aaee42489bfca84d70de3930f4a99c468
Dineth De Silva <dineth2022@gmail.com>
2021-10-25 19:27:20 +0530

    Minor changes

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ function main_default() {
     area.appendChild(main_page_area);
 }
 
-function main_addpage(Id, Title, Nposts, Desc) {
+function main_addpage(Noml, Id, Title, Nposts, Desc) {
     if (main_page_position_right) {
         var new_area_col = main_new_area_col();
-        var page = main_page(Id, Title, Nposts, Desc);
+        var page = main_page(Noml, Id, Title, Nposts, Desc);
         new_area_col.appendChild(page);
         main_page_area.appendChild(new_area_col);
         main_page_position_right = false;
@@ -27,48 +27,85 @@ function main_addpage(Id, Title, Nposts, Desc) {
         var cur_area_col = document.getElementById(
             "c" + main_current_col.toString()
         );
-        var page = main_page(Id, Title, Nposts, Desc);
+        var page = main_page(Noml, Id, Title, Nposts, Desc);
         cur_area_col.appendChild(page);
         main_page_position_right = true;
     }
 }
 
-function main_page(Id, Title, Nposts, Desc) {
-    var div = document.createElement("div");
-    div.style = "flex-grow: 1; display: flex; justify-content: center";
-    var div2 = document.createElement("div");
-    div2.classList.add("card");
-    div2.classList.add("main_page");
-    div2.classList.add("shadow");
-    var div3 = document.createElement("div");
-    div3.classList.add("card-body");
-    var h5 = document.createElement("h5");
-    h5.classList.add("card-title");
-    h5.style = "font-size: xx-large;";
-    h5.innerHTML =
-        "<span style='text-decoration: underline;'>" +
-        Title +
-        "</span> <span style='font-size: large;'>" +
-        "(" +
-        Nposts +
-        ")</span>";
-    div3.appendChild(h5);
-    div3.appendChild(document.createElement("br"));
-    var p = document.createElement("p");
-    p.classList.add("card-text");
-    p.style = "font-size: larger";
-    p.innerHTML = Desc;
-    div3.appendChild(p);
-    var a = document.createElement("a");
-    a.href = "https://dineth-de-silva.github.io/MyBlog/?pg=" + Id;
-    a.classList.add("btn");
-    a.classList.add("btn-outline-warning");
-    a.style = "border-radius: 50px";
-    a.innerHTML = "Posts <i class='fa fa-external-link'></i>";
-    div3.appendChild(a);
-    div2.appendChild(div3);
-    div.appendChild(div2);
-    return div;
+function main_page(Noml, Id, Title, Nposts, Desc) {
+    if (Noml){
+        var div = document.createElement("div");
+        div.style = "flex-grow: 1; display: flex; justify-content: center";
+        var div2 = document.createElement("div");
+        div2.classList.add("card");
+        div2.classList.add("main_page");
+        div2.classList.add("shadow");
+        var div3 = document.createElement("div");
+        div3.classList.add("card-body");
+        var h5 = document.createElement("h5");
+        h5.classList.add("card-title");
+        h5.style = "font-size: xx-large;";
+        h5.innerHTML =
+            "<span style='text-decoration: underline;'>" +
+            Title +
+            "</span> <span style='font-size: large;'>" +
+            "(" +
+            Nposts +
+            ")</span>";
+        div3.appendChild(h5);
+        div3.appendChild(document.createElement("br"));
+        var p = document.createElement("p");
+        p.classList.add("card-text");
+        p.style = "font-size: larger";
+        p.innerHTML = Desc;
+        div3.appendChild(p);
+        var a = document.createElement("a");
+        a.href = "https://dineth-de-silva.github.io/MyBlog/?pg=" + Id;
+        a.classList.add("btn");
+        a.classList.add("btn-outline-warning");
+        a.style = "border-radius: 50px";
+        a.innerHTML = "Posts <i class='fa fa-external-link'></i>";
+        div3.appendChild(a);
+        div2.appendChild(div3);
+        div.appendChild(div2);
+        return div;
+    }else{
+        var div = document.createElement("div");
+        div.style = "flex-grow: 1; display: flex; justify-content: center";
+        var div2 = document.createElement("div");
+        div2.classList.add("card");
+        div2.classList.add("main_page");
+        div2.classList.add("shadow");
+        div2.style = "background-color: rgb(255,0,0);width:250px;height:246"
+        var div3 = document.createElement("div");
+        div3.classList.add("card-body");
+        var h5 = document.createElement("h5");
+        h5.classList.add("card-title");
+        h5.style = "font-size: xx-large;";
+        h5.innerHTML =
+            "<span style='text-decoration: underline;'>All Posts</span> <span style='font-size: large;'>" +
+            "(" +
+            Nposts +
+            ")</span>";
+        div3.appendChild(h5);
+        div3.appendChild(document.createElement("br"));
+        // var p = document.createElement("p");
+        // p.classList.add("card-text");
+        // p.style = "font-size: larger";
+        // p.innerHTML = Desc;
+        // div3.appendChild(p);
+        var a = document.createElement("a");
+        a.href = "https://dineth-de-silva.github.io/MyBlog";
+        a.classList.add("btn");
+        a.classList.add("btn-dark");
+        a.style = "border-radius: 50px";
+        a.innerHTML = "View <i class='fa fa-external-link'></i>";
+        div3.appendChild(a);
+        div2.appendChild(div3);
+        div.appendChild(div2);
+        return div;
+    }
 }
 
 function main_new_area_col() {
@@ -82,6 +119,13 @@ function main_new_area_col() {
 function main_fetchpages() {
     firebase
         .database()
+        .ref("all/")
+        .on('value', (snapshot) => {
+            const Nposts = snapshot.val().nposts;
+            main_addpage(false, null,  null, Nposts, null);
+        });
+    firebase
+        .database()
         .ref("pages")
         .orderByChild("rid")
         .once("value", function (snapshot) {
@@ -90,7 +134,7 @@ function main_fetchpages() {
                 let Nposts = Childsnapshot.val().nposts;
                 let Title = Childsnapshot.val().title;
                 let Desc = Childsnapshot.val().desc;
-                main_addpage(Id, Title, Nposts, Desc);
+                main_addpage(true, Id, Title, Nposts, Desc);
             });
         });
 }
