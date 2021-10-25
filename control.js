d0bee1fdd188b948261fd254783b811790cbbab0
Dineth De Silva <dineth2022@gmail.com>
2021-10-25 13:30:59 +0530

    Created PWA

--- control.js
+++ control.js
@@ -1,3 +1,19 @@
+window.addEventListener("load", (e) => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ("serviceWorker" in navigator) {
+        try {
+            await navigator.serviceWorker.register("./service-worker.js");
+        } catch (e) {
+            alert("ServiceWorker registration failed. Sorry about that.");
+        }
+    } else {
+        document.querySelector(".alert").removeAttribute("hidden");
+    }
+}
+
 function open_menu() {
     var menu = document.getElementById("menu");
     menu.classList.remove("close");
